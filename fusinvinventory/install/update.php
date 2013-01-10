cd8116b00a0dcc414c4ef1c36d8afec5bfe6ebbc
Alexandre <adelaunay@teclib.com>
2013-01-10 15:32:17 +0100

    add more in TODO (see previous commit)

--- fusinvinventory/install/update.php
+++ fusinvinventory/install/update.php
@@ -40,9 +40,18 @@
    ------------------------------------------------------------------------
  */
 
+//TODO
 ALTER TABLE glpi_plugin_fusinvinventory_computers ADD COLUMN `vmname` VARCHAR(255) DEFAULT NULL;
 ALTER TABLE glpi_plugin_fusinvinventory_computers ADD COLUMN `vmid` VARCHAR(255) DEFAULT NULL;
 ALTER TABLE glpi_plugin_fusinvinventory_computers ADD COLUMN `vmfull` VARCHAR(255) DEFAULT NULL;
+
+We need to create two rules in top of rule motor : 
+1. "LPAR link (update)" : 
+   * criteria : "Assets to import : VMID+VMNAME" + "is already present in GLPI" + "yes"
+   * action : "Fusioninventory link" + "assign" + "Link if possible, else create device"
+2. "LPAR link (creation)"
+   * criteria : "Assets to import : VMID+VMNAME" + "exists" + "yes"
+   * action : "Fusioninventory link" + "assign" + "Link if possible, else create device"
 
 
 function pluginFusinvinventoryGetCurrentVersion($version) {
